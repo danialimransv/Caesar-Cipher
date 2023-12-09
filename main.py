221788b833589fd15da87afdab736f9059c5cd35
danialimransv <56647936+danialimransv@users.noreply.github.com>
2023-12-09 08:10:04 +0000

    Added user input

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-def caesar(message, key, mode):
+def caesar_cipher(message, key, mode):
   Letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   translated = ''
   message = message.upper()
   key = key % 26
-  
+
   for i in message:
-    tempLetter = Letter.find(i)
-    if mode == 'encrypt':
-      tempLetter += key
-     # print("encrypt")
-    elif mode == 'decrypt':
-     # print("decrypt")
-      tempLetter -= key
-    if tempLetter >= len(Letter):
-      tempLetter -= len(Letter)
-    elif tempLetter < 0:
-     tempLetter += len(Letter)
-    translated += Letter[tempLetter]
+      tempLetter = Letter.find(i)
+      if mode == 'encrypt':
+          tempLetter += key
+      elif mode == 'decrypt':
+          tempLetter -= key
+      if tempLetter >= len(Letter):
+          tempLetter -= len(Letter)
+      elif tempLetter < 0:
+          tempLetter += len(Letter)
+      translated += Letter[tempLetter]
   return translated
 
-print(caesar("BURGER", 6, 'encrypt'))
-print(caesar("HAXMKX", 6, 'decrypt'))
 
+# Allow user input
+user_message = input("Enter the message: ")
+user_key = int(input("Enter the key (an integer): "))
+user_mode = input("Enter the mode (encrypt/decrypt): ").lower()
+
+# Validate mode input
+if user_mode not in ['encrypt', 'decrypt']:
+  print("Invalid mode. Please enter 'encrypt' or 'decrypt'.")
+else:
+  result = caesar_cipher(user_message, user_key, user_mode)
+  print("Results:",result)
